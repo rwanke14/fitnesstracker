2642291f3df95ead3278759a9ade98e6982e22dd
Rachel Wanke <rkhorne09@gmail.com>
2021-03-18 00:57:11 -0400

    updating server for heroku

--- server.js
+++ server.js
@@ -19,10 +19,15 @@ app.use(express.json());
 app.use(express.static("public"));
 
 //Setting up connection to create collection and add seeders.
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+   mongoose.connect(
+     process.env.MONGODB_URI || 'mongodb://localhost/workout',
+     {
+       useNewUrlParser: true,
+       useUnifiedTopology: true,
+       useCreateIndex: true,
+       useFindAndModify: false
+     }
+   );
 
 //requiring routes for apis and for html.
 require("./routes/html-routes.js")(app);
